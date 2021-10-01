173b2f7766dac8e8ed7085d9fd9e21bd8cdcc437
Giancarlo <70995660+Gian0206@users.noreply.github.com>
2021-10-01 18:20:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import matrix_functions
 import random
 
 # A random matrix is going to be generated
-n_rows = random.randit(2,5)
+n_rows = random.randint(2,5)
 n_colums = random.randint(2,5)
 
-# numbers = list() *% #Pass for later adding 
-# row = list() *% #Pass for later
+numbers = list()
+row = list() 
 
 for i in range(n_rows):
     for j in range(n_colums):
-        row.append(rint(0,10))
+        row.append(random.randint(0,10))
     numbers.append(row)
     row = []
 
 # Prints the matrix generated
-print("Original:", numbers)
+print("Original:")
+for row in numbers:
+    print(row)
 
-tansposed = matrix_functions.transpose(numbers)
-print("\n"+"Transpose:", tansposed)
+transposed = matrix_functions.transpose(numbers)
+print("\n"+"Transpose:")
+for row in transposed:
+    print(row)
+
